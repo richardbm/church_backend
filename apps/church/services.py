94103b774715e725ebeda9d8f3c568d9902f3d3d
Richard Barrios <rhrichardbm@gmail.com>
2020-07-13 01:50:50 -0400

    feat(church): crate services for contact

--- apps/church/services.py
+++ apps/church/services.py
@@ -1,6 +1,6 @@
 from django.db.models import QuerySet
 
-from apps.church.models import About
+from apps.church.models import About, Contact
 
 
 def get_about_list() -> QuerySet:
@@ -9,3 +9,11 @@ def get_about_list() -> QuerySet:
 
 def get_about(about_id: int = None) -> About:
     return About.objects.filter(pk=about_id, is_active=True).first()
+
+
+def get_contact_list() -> QuerySet:
+    return Contact.objects.filter(is_active=True).order_by("order")
+
+
+def get_contact(contact_id: int = None) -> Contact:
+    return Contact.objects.filter(pk=contact_id, is_active=True).first()
