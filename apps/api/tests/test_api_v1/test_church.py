7b0c2016f71e273b5c51ebfcba2ab33c722b4f53
Richard Barrios <rhrichardbm@gmail.com>
2020-07-13 01:51:11 -0400

    feat(api.church): create tests for church api

--- apps/api/tests/test_api_v1/test_church.py
+++ apps/api/tests/test_api_v1/test_church.py
@@ -1,28 +1,31 @@
 from django.urls import reverse
-from rest_framework.test import APITestCase
+from model_bakery import baker
 from rest_framework import status
+from rest_framework.test import APITestCase
 
-from apps.accounts.models import User
-from apps.accounts.tests.bakery_recipes import user_john
-from apps.church.tests.bakery_recipes import about_mission, about_vision, about_history
+from apps.church.constants import (
+    CONTACT_PARAMETER_PHONE_NUMBER,
+    CONTACT_PARAMETER_EMAIL,
+)
+from apps.church.tests.bakery_recipes import (
+    about_mission,
+    about_vision,
+    about_history,
+    contact_workship,
+    contact_jane_doe,
+    contact_john_doe,
+)
 
 
 class AboutAPITestCase(APITestCase):
     def setUp(self) -> None:
         self.url_list = reverse("api:v1:church:about-list")
-        self.user_data = {
-            "email": "john@doe.com",
-            "first_name": "John",
-            "last_name": "Doe",
-            "password": "Doe",
-            "phone_number": "+56 943532346",
-        }
 
     def test_get_about_list(self):
         history = about_history.make(order=3)
         vision = about_vision.make(order=2)
         mission = about_mission.make(order=1)
-        response = self.client.get(self.url_list, data=self.user_data)
+        response = self.client.get(self.url_list)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
         self.assertEqual(len(response.data), 3)
         self.assertEqual(response.data[0]["label"], mission.label, response.data)
@@ -37,3 +40,43 @@ class AboutAPITestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
         self.assertEqual(response.data["label"], mission.label)
         self.assertEqual(response.data["description"], mission.description)
+
+
+class ContactAPITestCase(APITestCase):
+    def setUp(self) -> None:
+        self.url_list = reverse("api:v1:church:contact-list")
+
+    def test_get_contact_list(self):
+        workship = contact_workship.make(order=3)
+        jane_doe = contact_jane_doe.make(order=2)
+        john_doe = contact_john_doe.make(order=1)
+        response = self.client.get(self.url_list)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+        self.assertEqual(len(response.data), 3)
+        self.assertEqual(response.data[0]["name"], john_doe.name, response.data)
+        self.assertEqual(response.data[1]["name"], jane_doe.name, response.data)
+        self.assertEqual(response.data[2]["name"], workship.name, response.data)
+
+    def test_get_about_detail(self):
+        john_doe = contact_john_doe.make(order=1)
+        contact_jane_doe.make(order=2)
+        baker.make(
+            "church.ContactParameter",
+            label=CONTACT_PARAMETER_PHONE_NUMBER,
+            value="+56 912345678",
+            contact=john_doe,
+        )
+
+        baker.make(
+            "church.ContactParameter",
+            label=CONTACT_PARAMETER_EMAIL,
+            value="john@doe.com",
+            contact=john_doe,
+        )
+
+        url = reverse("api:v1:church:contact-detail", kwargs={"pk": john_doe.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+        self.assertEqual(response.data["name"], john_doe.name)
+        self.assertEqual(response.data["description"], john_doe.description)
+        self.assertEqual(len(response.data["contact_parameters"]), 2)
--- apps/church/tests/bakery_recipes.py
+++ apps/church/tests/bakery_recipes.py
@@ -1,5 +1,6 @@
 from model_bakery.recipe import Recipe
 
+# ############### About ###############
 
 about_mission = Recipe(
     "church.About",
@@ -20,4 +21,20 @@ about_history = Recipe(
     label="History",
     description="This is the history of the church...",
     order=1,
+)
+
+
+# ############### Contact ###############
+
+
+contact_john_doe = Recipe(
+    "church.Contact", name="John Doe", description="Main Pastor", order=1
+)
+
+contact_jane_doe = Recipe(
+    "church.Contact", name="Jane Doe", description="Elder", order=2
+)
+
+contact_workship = Recipe(
+    "church.Contact", name="Workship ministry", description="", order=3
 )
