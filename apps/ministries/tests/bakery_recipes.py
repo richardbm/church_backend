3bc73c9e0e21577a946846d8310c3651140f4436
Richard Barrios <rhrichardbm@gmail.com>
2020-07-19 21:30:33 -0400

    feat(ministries): create recipe for Ministry

--- apps/ministries/tests/bakery_recipes.py
+++ apps/ministries/tests/bakery_recipes.py
@@ -1,1 +1,12 @@
-from model_bakery.recipe import Recipe
+from model_bakery.recipe import Recipe, foreign_key
+
+from apps.church.tests.bakery_recipes import contact_biblical_school
+from apps.ministries.models import Ministry
+
+
+ministry_biblical_school = Recipe(
+    Ministry,
+    name="Biblical School",
+    description="The biblical school...",
+    contact_information=foreign_key(contact_biblical_school)
+)
