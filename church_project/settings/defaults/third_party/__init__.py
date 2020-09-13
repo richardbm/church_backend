5ef3cf78f17d1aef44e82c59795ed06174328e06
Richard Barrios <richard@cornershopapp.com>
2020-09-13 19:19:06 -0300

    chore(corsheaders): allow all origins

--- church_project/settings/defaults/third_party/__init__.py
+++ church_project/settings/defaults/third_party/__init__.py
@@ -2,4 +2,3 @@ from .rest_framework import *
 from .oauth2_provider import *
 from .celery import *
 from .corsheaders import *
-
--- church_project/settings/defaults/third_party/corsheaders.py
+++ church_project/settings/defaults/third_party/corsheaders.py
@@ -1,1 +1,2 @@
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = ["http://*", "https://*"]
