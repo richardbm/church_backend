7591251b2b3eace7b9ab87c06c99a3dba1858e9d
Richard Barrios <richard@cornershopapp.com>
2020-09-13 19:13:27 -0300

    chore(django): allow any origin

--- church_project/settings/defaults/django.py
+++ church_project/settings/defaults/django.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'c0)z)db_6k!c)8@l5r2f7(2k(_zlso8=+mwtwxpfoqzw5og$$z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = "*"
 
 
 # Application definition
