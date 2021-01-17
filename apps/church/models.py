9729adb1fd06108b37c7a07264e819248a64c446
Richard Barrios <richard@cornershopapp.com>
2021-01-17 20:37:42 -0300

    feat(church): add models for embership

--- apps/church/models.py
+++ apps/church/models.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 from django.db import models
+from django.utils.translation import ugettext_lazy
+from phonenumber_field.modelfields import PhoneNumberField
 
 from apps.church.constants import (
     CONTACT_PARAMETER_ADDRESS,
@@ -55,3 +59,41 @@ class News(CustomModel):
 
     def __str__(self):
         return self.subject
+
+
+class Membership(CustomModel):
+    first_name = models.CharField(ugettext_lazy("first name"), max_length=30)
+    last_name = models.CharField(ugettext_lazy("last name"), max_length=150)
+    email = models.EmailField()
+    phone_number = PhoneNumberField(blank=True, null=True)
+    member_since = models.DateTimeField(default=datetime.now)
+    user = models.OneToOneField(
+        "accounts.User",
+        blank=True,
+        null=True,
+        related_name="user",
+        on_delete=models.SET_NULL,
+    )
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return f"{self.id} - {self.full_name}"
+
+
+class MemberMinistry(CustomModel):
+    MEMBER_TYPE_CHOICE = (
+        ("DISCIPLE", ugettext_lazy("CHURCH_MEMBER_TYPE_DISCIPLE")),
+        ("COLLABORATOR", ugettext_lazy("CHURCH_MEMBER_TYPE_COLLABORATOR")),
+        ("LEADER", ugettext_lazy("CHURCH_MEMBER_TYPE_LEADER")),
+    )
+    member = models.ForeignKey(
+        Membership, related_name="ministries", on_delete=models.CASCADE
+    )
+    ministry = models.ForeignKey(
+        "ministries.Ministry", related_name="members", on_delete=models.PROTECT
+    )
+    member_type = models.CharField(max_length=16, choices=MEMBER_TYPE_CHOICE)
+    is_baptized = models.BooleanField()
